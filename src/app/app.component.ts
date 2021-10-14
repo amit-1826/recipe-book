00300fb9c3196f32840dd468e22f65860bef01ca
amit-1826 <amitdubey1826@gmail.com>
2021-10-14 12:53:32 +0530

    Added angular universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 import { AuthService } from './modules/auth/auth.service';
 import * as fromApp from '../app/store/appReducer';
 import { Store } from '@ngrx/store';
@@ -12,12 +13,16 @@ import * as AuthActions from '../app/modules/auth/store/auth.actions';
 export class AppComponent implements OnInit {
 
   constructor(private authService: AuthService,
+              @Inject(PLATFORM_ID) private platFormId,
               private store: Store<fromApp.AppState>) {
 
   }
 
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platFormId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
+    console.log('inside app component');
   }
 
 }
